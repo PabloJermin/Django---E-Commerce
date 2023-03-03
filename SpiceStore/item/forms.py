7517729e856f972699e4274e91a8c16c6afed8e0
PabloJermin <jerminamartey@gmail.com>
2023-03-03 02:54:38 -0800

    login and adding items

---
+++ SpiceStore/item/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Item
+
+CLASS_VARIABLE = 'w-full py-4 px-6 rounded-xl border'
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category','name', 'description', 'image', 'price')
+        
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': CLASS_VARIABLE
+            }),
+            'name': forms.TextInput(attrs={
+                'class': CLASS_VARIABLE
+            }),
+            'Description': forms.Textarea(attrs={
+                'class': CLASS_VARIABLE
+            }),
+            'price': forms.TextInput(attrs={
+                'class': CLASS_VARIABLE
+            }),
+            'image': forms.FileInput(attrs={
+                'class': CLASS_VARIABLE
+            }),
+             
+        }
