ee512e5bf28155738422b0665f6e01462bc4fd85
PabloJermin <jerminamartey@gmail.com>
2023-03-03 08:21:43 -0800

    edit and delete tags added

---
+++ SpiceStore/Dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'Dashboard'
+
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('<int:pk>/', views.detail, name='delete')
+    
+]
